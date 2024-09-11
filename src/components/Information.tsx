a8dc7cab21c84c5498493e07dcd19ee4b7f15467
javierguerra2203 <javierguerra2203@gmail.com>
2024-09-11 13:56:59 +0000

    (fix): code formatted

--- src/components/Information.tsx
+++ src/components/Information.tsx
@@ -1,85 +1,93 @@
 import { Card, CardContent } from "@/components/ui/card";
 import { INFORMATION } from "@/data/information";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { ChevronDown } from 'lucide-react';
+import { ChevronDown } from "lucide-react";
 import BlurFade from "./magicui/blur-fade";
 
 export function Information() {
-    return (
-        <section>
-            <BlurFade delay={0.25 * 6}>
-                <div className="flex flex-col justify-center items-center">
-                    <p className="text-xl font-bold text-center">Encuentra <span className="text-[#70b22d]">información relevante</span> sobre el tecnólogo aquí.</p>
-                    <ChevronDown className="text-center animate-bounce mt-2 mb-2" />
-                </div>
-            </BlurFade>
-            <BlurFade delay={0.25 * 7}>
-                <Tabs defaultValue="info" className="w-full">
-                    <TabsList className="grid w-full grid-cols-3">
-                        <TabsTrigger value="info">Información</TabsTrigger>
-                        <TabsTrigger value="desc">Descripción</TabsTrigger>
-                        <TabsTrigger value="req">Requisitos</TabsTrigger>
-                    </TabsList>
-                    <TabsContent value="info">
-                        <Card>
-                            <CardContent className="grid grid-cols-1 md:grid-cols-2 gap-4 p-6">
-                                <div>
-                                    <p className="text-gray-700">
-                                        <strong className="text-gray-900">Ciudad: </strong>{
-                                            INFORMATION.city
-                                        }
-                                    </p>
-                                    <p className="text-gray-700 text-balance">
-                                        <strong className="text-gray-900">Lugar de Realización: </strong>
-                                        {INFORMATION.realization_place}
-                                    </p>
-                                    <p className="text-gray-700">
-                                        <strong className="text-gray-900">Modalidad: </strong>{
-                                            INFORMATION.modality
-                                        }
-                                    </p>
-                                    <p className="text-gray-700">
-                                        <strong className="text-gray-900">Cupo Máximo: </strong>
-                                        {INFORMATION.max_capacity}
-                                    </p>
-                                </div>
-                                <div>
-                                    <p className="text-gray-700">
-                                        <strong className="text-gray-900">Tipo de Programa: </strong>{INFORMATION.program_type}
-                                    </p>
-                                    <p className="text-gray-700">
-                                        <strong className="text-gray-900">Perfil de Entrada: </strong>{INFORMATION.entry_profile}
-                                    </p>
-                                    <p className="text-gray-700">
-                                        <strong className="text-gray-900">Línea o Área: </strong>{
-                                            INFORMATION.area
-                                        }
-                                    </p>
-                                </div>
-                            </CardContent>
-                        </Card>
-                    </TabsContent>
-                    <TabsContent value="desc">
-                        <Card>
-                            <CardContent className="p-6">
-                                <p className="text-gray-700 text-pretty md:text-lg">
-                                    {INFORMATION.description}
-                                </p>
-                            </CardContent>
-                        </Card>
-                    </TabsContent>
-                    <TabsContent value="req">
-                        <Card>
-                            <CardContent className="p-6">
-                                <p className="text-gray-700 text-balance md:text-lg">
-                                    {INFORMATION.extra_requirements}
-                                </p>
-                            </CardContent>
-                        </Card>
-                    </TabsContent>
-                </Tabs>
-            </BlurFade>
-        </section>
-
-    )
+	return (
+		<section>
+			<BlurFade delay={0.25 * 6}>
+				<div className="flex flex-col items-center justify-center">
+					<p className="text-center text-xl font-bold">
+						Encuentra{" "}
+						<span className="text-[#70b22d]">información relevante</span> sobre
+						el tecnólogo aquí.
+					</p>
+					<ChevronDown className="mb-2 mt-2 animate-bounce text-center" />
+				</div>
+			</BlurFade>
+			<BlurFade delay={0.25 * 7}>
+				<Tabs defaultValue="info" className="w-full">
+					<TabsList className="grid w-full grid-cols-3">
+						<TabsTrigger value="info">Información</TabsTrigger>
+						<TabsTrigger value="desc">Descripción</TabsTrigger>
+						<TabsTrigger value="req">Requisitos</TabsTrigger>
+					</TabsList>
+					<TabsContent value="info">
+						<Card>
+							<CardContent className="grid grid-cols-1 gap-4 p-6 md:grid-cols-2">
+								<div>
+									<p className="text-gray-700">
+										<strong className="text-gray-900">Ciudad: </strong>
+										{INFORMATION.city}
+									</p>
+									<p className="text-balance text-gray-700">
+										<strong className="text-gray-900">
+											Lugar de Realización:{" "}
+										</strong>
+										{INFORMATION.realization_place}
+									</p>
+									<p className="text-gray-700">
+										<strong className="text-gray-900">Modalidad: </strong>
+										{INFORMATION.modality}
+									</p>
+									<p className="text-gray-700">
+										<strong className="text-gray-900">Cupo Máximo: </strong>
+										{INFORMATION.max_capacity}
+									</p>
+								</div>
+								<div>
+									<p className="text-gray-700">
+										<strong className="text-gray-900">
+											Tipo de Programa:{" "}
+										</strong>
+										{INFORMATION.program_type}
+									</p>
+									<p className="text-gray-700">
+										<strong className="text-gray-900">
+											Perfil de Entrada:{" "}
+										</strong>
+										{INFORMATION.entry_profile}
+									</p>
+									<p className="text-gray-700">
+										<strong className="text-gray-900">Línea o Área: </strong>
+										{INFORMATION.area}
+									</p>
+								</div>
+							</CardContent>
+						</Card>
+					</TabsContent>
+					<TabsContent value="desc">
+						<Card>
+							<CardContent className="p-6">
+								<p className="text-pretty text-gray-700 md:text-lg">
+									{INFORMATION.description}
+								</p>
+							</CardContent>
+						</Card>
+					</TabsContent>
+					<TabsContent value="req">
+						<Card>
+							<CardContent className="p-6">
+								<p className="text-balance text-gray-700 md:text-lg">
+									{INFORMATION.extra_requirements}
+								</p>
+							</CardContent>
+						</Card>
+					</TabsContent>
+				</Tabs>
+			</BlurFade>
+		</section>
+	);
 }
--- src/data/information.ts
+++ src/data/information.ts
@@ -1,27 +1,29 @@
 export const INFORMATION = {
-    city: 'Valledupar',
-    realization_place: 'CIGEC - Carrera 19 entre Calles 14 y 15',
-    modality: 'Presencial',
-    max_capacity: '30',
-    program_type: 'Carrera Técnica',
-    entry_profile: 'Media Académica',
-    area: 'Cliente',
-    description: 'Este curso está diseñado para desarrollar habilidades en la gestión documental de acuerdo con las normativas y procedimientos técnicos vigentes, aplicando prácticas de protección ambiental, seguridad y salud en el trabajo. El egresado será capaz de manejar archivos, digitalizar documentos y gestionar bases de datos conforme a procedimientos archivísticos y tecnológicos.',
-    abilities: [
-        'Aplicar prácticas de protección ambiental, seguridad y salud en el trabajo.', 
-        'Controlar documentos de archivo según la normativa archivística.',
-        'Digitalizar documentos de acuerdo con procedimientos técnicos.',
-        'Interactuar en inglés de forma oral y escrita dentro de contextos laborales.',
-        'Desarrollar procesos de comunicación eficaz en contextos productivos.',
-        'Alimentar bases de datos conforme a procedimientos técnicos.',
-        'Dirigir el talento humano y gestionar procesos de cultura emprendedora.'
-    ],
-    themes: [
-        'Organizar archivos de gestión y fondos documentales.',
-        'Procesar datos estadísticos con procedimientos archivísticos.',
-        'Utilizar herramientas informáticas para la gestión documental.',
-        'Orientar investigaciones formativas según referentes técnicos.',
-        'Gestionar correspondencia con procesos técnicos y normativos.'
-    ],
-    extra_requirements: 'Presentar el resultado de la prueba o examen de estado de la educación media.'
-}
+	city: "Valledupar",
+	realization_place: "CIGEC - Carrera 19 entre Calles 14 y 15",
+	modality: "Presencial",
+	max_capacity: "30",
+	program_type: "Carrera Técnica",
+	entry_profile: "Media Académica",
+	area: "Cliente",
+	description:
+		"Este curso está diseñado para desarrollar habilidades en la gestión documental de acuerdo con las normativas y procedimientos técnicos vigentes, aplicando prácticas de protección ambiental, seguridad y salud en el trabajo. El egresado será capaz de manejar archivos, digitalizar documentos y gestionar bases de datos conforme a procedimientos archivísticos y tecnológicos.",
+	abilities: [
+		"Aplicar prácticas de protección ambiental, seguridad y salud en el trabajo.",
+		"Controlar documentos de archivo según la normativa archivística.",
+		"Digitalizar documentos de acuerdo con procedimientos técnicos.",
+		"Interactuar en inglés de forma oral y escrita dentro de contextos laborales.",
+		"Desarrollar procesos de comunicación eficaz en contextos productivos.",
+		"Alimentar bases de datos conforme a procedimientos técnicos.",
+		"Dirigir el talento humano y gestionar procesos de cultura emprendedora.",
+	],
+	themes: [
+		"Organizar archivos de gestión y fondos documentales.",
+		"Procesar datos estadísticos con procedimientos archivísticos.",
+		"Utilizar herramientas informáticas para la gestión documental.",
+		"Orientar investigaciones formativas según referentes técnicos.",
+		"Gestionar correspondencia con procesos técnicos y normativos.",
+	],
+	extra_requirements:
+		"Presentar el resultado de la prueba o examen de estado de la educación media.",
+};
